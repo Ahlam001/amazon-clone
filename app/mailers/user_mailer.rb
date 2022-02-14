0136baa5d1429d0aaafa812678ab55562b2a11a2
Ahlam <ahlam@Ahlams-MacBook-Pro.local>
2022-02-15 00:08:42 +0300

    created send mail job

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,9 @@
 class UserMailer < ApplicationMailer
 
-    def new_user_email
-        @user = params[:user]
+
+    def new_user_email(id)
+       
+        @user =  User.find(id)
 
        mail(to: "aalmutairi@mrsool.co", subject: "New user created")
     end
---
+++ test/jobs/sending_mail_job_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class SendingMailJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
